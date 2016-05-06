8eec0aefcfbbb82b8b415ea05e97c33c2b467f55
Радж Мамедов <idzacrash@gstu.by>
2016-05-06 13:17:54 +0300

    Changed a logic for calculation sum of digits from strings; Added commaColonSON function

--- Task#1/statements.js
+++ Task#1/statements.js
@@ -1,35 +1,58 @@
 ;(function(){
-	
+
 	// Sum of digits from strings
-	function getDigitsFromStr(str) {
-		return str.match(/0x[A-F0-9]+|\-?[0-9]+\.?[0-9]*(e-?[0-9]+)*/gi);
-	}
+	function getDigitFromString(str) {
+		if (!str) return 0;
 
-	function safeSummarize(arr) {
-		// Match can return null, so check it
-		if (!arr) return 0;
+		// Match can return null, so check it or give default value
+		var digits = str.match(/(0x)?[A-F0-9]+/gi),
+			hexPtrn = /(0x|[A-F])/i;
 
-		return arr.reduce(function(res, cur){
-			return res + parseInt(cur);
-		}, 0);
+		if (!digits) return 0;
+
+		return +digits.reduce(function(res, cur){
+			var num = hexPtrn.test(cur) ? parseInt(cur, 16) : parseInt(cur);
+			
+			return res + num;
+		}, '');
 	}
 
 	function sumDigitsFromStrings(str1, str2) {
-		var digitsSet1 = getDigitsFromStr(str1),
-			digitsSet2 = getDigitsFromStr(str2);
+		var digitFromStr1 = getDigitFromString(str1),
+			digitFromStr2 = getDigitFromString(str2);
 
-		var digitsSet1Sum = safeSummarize(digitsSet1),
-			digitsSet2Sum = safeSummarize(digitsSet2);
+		return digitFromStr1 + digitFromStr2;
+	}
+	
+	// CommaColonSON
+	function commaColonSON(str) {
+		if (typeof str != 'string') return;
 
-		return digitsSet1Sum + digitsSet2Sum;
+		var res = {},
+			nextRow,
+			ptrn = /(\w+)\s*,\s*(\w+)/g;
+
+		while(nextRow = ptrn.exec(str)) {
+			res[nextRow[1]] = nextRow[2];
+		}
+
+		return res;
 	}
 
 	// Tests
 	// -- Sum of digits from strings
+	console.log('Testing ---> Sum of digits from strings');
 	console.log(sumDigitsFromStrings(
-		'12every2e-1thing is0xA5F so comple0x1c1G',
+		'12every2e-1thing is0x10 so comple0x1G',
 		'01every45-e12e2day 0.1is like 1.a-3 5t5est'));
 	console.log(sumDigitsFromStrings('1 2 3', 'a5 b6 c-17'));
 	console.log(sumDigitsFromStrings('___', '0'));
+	console.log(sumDigitsFromStrings('', ''));
+	console.log(sumDigitsFromStrings('123x1z13', 'a123'));
+
+	// CommaColonSON
+	console.log('\nTesting ---> CommaColonSON');
+	console.log(commaColonSON('17,minsk;232,gomel;162,brest;212,vitebsk;1522,grodno;222,mogilev'));
+	console.log(commaColonSON('key')); // is it right? or it should output {key: ''}?
 
 })();
